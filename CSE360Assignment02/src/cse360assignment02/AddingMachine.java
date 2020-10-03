c19be43811f1a6c9df9ae15cd5df7bbb89793b3c
zengkeatgiam <zengkeatgiam@zengs-mbp>
2020-10-03 21:32:59 +0800

    adding Javadoc style comment into the code

--- CSE360Assignment02/src/cse360assignment02/AddingMachine.java
+++ CSE360Assignment02/src/cse360assignment02/AddingMachine.java
@@ -3,6 +3,10 @@ package cse360assignment02;
 public class AddingMachine {
 	
 	private int total;
+	
+	/**
+	 * adding a private string to store the transaction history
+	 */
 	private String result = "";
 	
 	public static void main(String[] args) {
@@ -12,19 +16,27 @@ public class AddingMachine {
 		myCalculator.add(4);
 		myCalculator.subtract(2);
 		myCalculator.add(5);
+		
+		/**
+		 * call toString() method to return the string
+		 */
 		System.out.print(myCalculator);
 	}
 	
 	  
 	  public AddingMachine () {
 		 total = 0;
-		 result = " 0 ";
+		 result = "0";
 	  }
 	  
 	  public int getTotal () {
 	    return total;
 	  }
 	  
+		/**
+		 * adding the value to total and store the value
+		 * into the result string.
+		 */
 	  public void add (int value) {
 		  
 		  total = total + value;
@@ -36,11 +48,19 @@ public class AddingMachine {
 		  total = total - value;
 		  result = result + " - " + value;
 	  }
-
+	  
+	  
+		/**
+		 * return the result string
+		 */
 	  public String toString () {
 	    return result.toString();
 	  }
-
+	  
+		/**
+		 * no description in assignment describe 
+		 * what this method should perform
+		 */
 	  public void clear() {
 	  }
 }
