ac12ca46e6665519ef568efd046fc8eaa46f1229
zengkeatgiam <zengkeatgiam@zengs-mbp>
2020-10-03 21:26:27 +0800

    Added all the required functionality and variable to run the code

--- CSE360Assignment02/src/cse360assignment02/AddingMachine.java
+++ CSE360Assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,25 +1,44 @@
 package cse360assignment02;
 
 public class AddingMachine {
+	
 	private int total;
+	private String result = "";
+	
+	public static void main(String[] args) {
+		
+		AddingMachine myCalculator = new AddingMachine();
+		
+		myCalculator.add(4);
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		System.out.print(myCalculator);
+	}
+	
 	  
 	  public AddingMachine () {
-		  
-	    total = 1;  // not needed - included for clarity
+		 total = 0;
+		 result = " 0 ";
 	  }
 	  
 	  public int getTotal () {
-	    return 0;
+	    return total;
 	  }
 	  
 	  public void add (int value) {
+		  
+		  total = total + value;
+		  result = result + " + " + value;
 	  }
 
 	  public void subtract (int value) {
+		  
+		  total = total - value;
+		  result = result + " - " + value;
 	  }
 
 	  public String toString () {
-	    return "";
+	    return result.toString();
 	  }
 
 	  public void clear() {
